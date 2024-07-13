df5e8567e639b79ba8cf7d62263e3e82fbf5a1e5
Rusakov24 <lovewillkillme@vk.com>
2024-07-13 14:17:57 +0300

    Начало

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задание 1");
+        int[] numbers = new int[]{1, 2, 3};
+        double[] = {1.57, 7.654,9.986}
+
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+
+        System.out.println("Задание 2");
+
+        }
+    }
