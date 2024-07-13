4993ea3e5b71b6c8ae79f8b9bba7e5a8104a6013
Rusakov24 <lovewillkillme@vk.com>
2024-07-14 00:57:41 +0300

    Конец 2

--- src/Main.java
+++ src/Main.java
@@ -94,7 +94,7 @@ public class Main {
                 System.out.print(array[i] + ", ");
             }
         }
-        System.out.println("\nЗадание 4");
+        System.out.println("\nЗадание 4 ");
         int[]  numbers = {0,1,2,3,4,5,6,7,8,9 };
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] % 2 != 0) {
